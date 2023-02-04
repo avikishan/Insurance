a3d80176d0bcb46ce8c39b4e887a45b96793a96b
Abhineash Kishan <avikishan258@gmail.com>
2023-02-05 04:06:13 +0530

    utils file, config file updated

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from Insurance.logger import logging
 from Insurance.exception import InsuranceException
 import os,sys
-
-def test_logger_and_exception():
-    try:
-        logging.info("Startingt the test_logger_and_exception")
-        result=3/0
-        print(result)
-        logging.info("Ending point of the test_logger_and_exception")
-    except Exception as e:
-        logging.debug(str(e))
-        raise InsuranceException(e,sys)
+from Insurance.utils import get_collections_as_dataframe
+# def test_logger_and_exception():
+#     try:
+#         logging.info("Startingt the test_logger_and_exception")
+#         result=3/0
+#         print(result)
+#         logging.info("Ending point of the test_logger_and_exception")
+#     except Exception as e:
+#         logging.debug(str(e))
+#         raise InsuranceException(e,sys)
     
 
 if __name__=="__main__":
     try:
-        test_logger_and_exception()
+        # test_logger_and_exception()
+        get_collections_as_dataframe(database_name="INSURANCE",collection_name="INSURANCE_PROJECT")
     except Exception as e:
         print(e)
